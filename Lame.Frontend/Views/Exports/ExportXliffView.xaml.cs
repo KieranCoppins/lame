0c72d6a0593e4027f4e9ef4f0f263271a9267a64
Kieran Coppins <kierancoppins2000@gmail.com>
2026-03-11 08:53:44 +0000

    Filter english from target language selector in xliff export

--- Lame.Frontend/Views/Exports/ExportXliffView.xaml.cs
+++ Lame.Frontend/Views/Exports/ExportXliffView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Data;
+using Lame.DomainModel;
 
 namespace Lame.Frontend.Views.Exports;
 
@@ -7,5 +9,14 @@ public partial class ExportXliffView : UserControl
     public ExportXliffView()
     {
         InitializeComponent();
+
+        var cvs = (CollectionViewSource)Resources["FilteredLanguages"];
+        cvs.Filter += (s, e) =>
+        {
+            if (e.Item is Language lang)
+                e.Accepted = lang.LanguageCode != "en";
+            else
+                e.Accepted = false;
+        };
     }
 }
