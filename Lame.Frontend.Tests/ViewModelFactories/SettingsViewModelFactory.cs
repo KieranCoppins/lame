b56f56177eb2736eaeda6a6395804e16422bfbc5
Kieran Coppins <kierancoppins2000@gmail.com>
2026-03-24 09:16:49 +0000

    Created unit test

--- Lame.Frontend.Tests/ViewModelFactories/SettingsViewModelFactory.cs
+++ Lame.Frontend.Tests/ViewModelFactories/SettingsViewModelFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Lame.Backend.Languages;
+using Lame.Frontend.Models;
 using Lame.Frontend.Services;
 using Lame.Frontend.ViewModels;
 using Moq;
@@ -13,10 +14,22 @@ public class SettingsViewModelFactory
         IUserSettingsService? userSettingsService = null
     )
     {
+        // Constructor requires user settings have an object, lets mock it
+        if (userSettingsService == null)
+        {
+            var userSettingsServiceMock = new Mock<IUserSettingsService>();
+            userSettingsServiceMock.Setup(x => x.UserSettings)
+                .Returns(new UserSettings
+                {
+                    BaseDirectory = "Initial Directory"
+                });
+            userSettingsService = userSettingsServiceMock.Object;
+        }
+
         return new SettingsViewModel(
             dialogService ?? new Mock<IDialogService>().Object,
             languagesService ?? new Mock<ILanguages>().Object,
-            userSettingsService ?? new Mock<IUserSettingsService>().Object
+            userSettingsService
         );
     }
 }
