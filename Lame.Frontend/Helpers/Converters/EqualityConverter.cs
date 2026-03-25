6ab92ad4221956edf7ce709bafab1ec33299e655
Kieran Coppins <kierancoppins2000@gmail.com>
2026-03-25 20:05:21 +0000

    Change log view provides pagination navigation of entire change log

---
+++ Lame.Frontend/Helpers/Converters/EqualityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Lame.Frontend.Helpers.Converters;
+
+public class EqualityConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values == null || values.Length < 2)
+            return false;
+
+        var first = values[0];
+        var second = values[1];
+
+        if (first == null && second == null)
+            return true;
+
+        if (first == null || second == null)
+            return false;
+
+        return first.Equals(second);
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
